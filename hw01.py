ad0a878e364c438f44f1dd667ee67075434bab13
Kiku Nagai-Velasquez <knagaivelasquez@smith.edu>
2026-09-20 19:43:50 -0400

    done!

--- hw01.py
+++ hw01.py
@@ -1,7 +1,7 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
-#       Peers: (add any collaborators)
-#  References: (anything you checked to solve this)
+#        Name: (KIKU NAGAI-VELASQUEZ)
+#       Peers: (NONE)
+#  References: (NONE)
 # ------------------------------------------------------
 
 
@@ -43,23 +43,6 @@ def main():
 
 
     # Part 1: Basic Operations
-    # =============================================
-    # Your code for part 1 under this line and before the print statements
-    x= 27
-    y= 1
-    a = 1.5
-    b=7
-    c=-1
-    result= 3.0
-    
-    print("Part 1: x =", x)
-    print("Part 1: y =", y)
-    print("Part 1: a =", a)
-    print("Part 1: b =", b)
-    print("Part 1: c =", c)
-    print("Part 1: result =", result)
-    
-
 #============================================
         #the following must be printed:
 #Part 1: x = 27
@@ -68,30 +51,78 @@ def main():
 #Part 1: b = 7
 #Part 1: c = -1
 #Part 1: result = 3.0
-#===============================================
-    # End of Part 1 ----------------------
+# =============================================
+    # Your code for part 1 under this line and before the print statements
+    
+    x= 27
+    y= 1
+    a = 1.5
+    b=7
+    c=-1
+
+    result1= ((3*x)-(9*y))/((2*a)*(b-c))
+    
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
+    
+
+# End of Part 1 -------------------------------------------------------------------------------------
 
 
     # Part 2: Power
-    # =============================================
+#=============================================
+        #the following must be printed:
+#Part 2: x = 5
+#Part 2: y = -3
+#Part 2: result = 2025
+#=========================================
     # Your code for part 2 under this line and before the print statements
-
-    # End of Part 2 ----------------------
+    x =5
+    y=-3
+    result2 =(x**2)*(y**4)
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
+    
+# End of Part 2 ----------------------------------------------------------------------------------------
 
 
 
     # Part 3: Integer divide
-    # =============================================
-    # Your code for part 3 under this line and before the print statements
+#============================================
+        #the following must be printed:
+#Part 3: a = 100
+#Part 3: b = 13
+#Part 3: result = 7
+#==============================================
 
-    # End of Part 3 ----------------------
+    # Your code for part 3 under this line and before the print statements
+    a= 100
+    b = 13
+    result3= a//b
+    
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result3)
+    
+
+# End of Part 3 -----------------------------------------------------------------------------
 
 
     # Part 4: Modulo
-    # =============================================
-    # Your code for part 4 under this line and before the print statements
+#=============================================
+      #the following must be printed:
+#Part 4: result = 9
+#==============================================
+# % is used for modulo!!! (gives remainder of dog treats)
+    result4= a%b
+    print("Part 4: result =", result4)    # Your code for part 4 under this line and before the print statements
 
-    # End of Part 4 ----------------------
+# End of Part 4 -------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     main()
